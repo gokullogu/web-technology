a027be73d221148155f8c8b8e22616cbca881057
gokullogu <gokulpollachi25@gmail.com>
2021-08-16 20:48:17 +0530

    ex3

---
+++ res/ex3/formvalid.js
@@ -0,0 +1,66 @@
+function validation()                                    
+{ 
+    var name = document.forms["RegForm"]["Name"];               
+    var email = document.forms["RegForm"]["EMail"];    
+    var phone = document.forms["RegForm"]["Telephone"];  
+    var what =  document.forms["RegForm"]["Subject"];  
+    var password = document.forms["RegForm"]["Password"];  
+    var address = document.forms["RegForm"]["Address"];  
+    var letters=/^[A-Za-z]+$/;
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    } 
+    if (!name.value.match(letters))
+    {
+        window.alert("Enter Alphabets only");
+        name.focus(); 
+        return false; 
+    }
+    if (address.value == "")                               
+    { 
+        window.alert("Please enter your address."); 
+        address.focus(); 
+        return false; 
+    } 
+       
+    if (email.value == "")                                   
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (phone.value == "")                           
+    { 
+        window.alert("Please enter your telephone number."); 
+        phone.focus(); 
+        return false; 
+    } 
+    if (isNaN(phone.value)||phone.value<7000000000 ||phone.value>9999999999)
+    {
+
+       window.alert("Please  enter a correct phone number");
+        phone.focus(); 
+        return false; 
+     }
+   
+    if (password.value == "")                        
+    { 
+        window.alert("Please enter your password"); 
+        password.focus(); 
+        return false; 
+    } 
+   
+    if (what.selectedIndex < 1)                  
+    { 
+        alert("Please enter your course."); 
+        what.focus(); 
+        return false; 
+    } 
+    
+    alert("Thanks for filling the form");
+    return true; 
+}
